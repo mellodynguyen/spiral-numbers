c6bc141179fa863fe1889a9e0045b546a9845ee5
mello <mello@LAPTOP-QG5OOSFN>
2025-09-13 19:04:19 -0500

    Add project files.

---
+++ ElmoSquareMN/ElmoSquareMN.py
@@ -0,0 +1,15 @@
+"""
+Mellody Nguyen
+COSC 2436
+Program Set 1
+"""
+# String Number Program
+# Input two numbers, range 1-100, separated by space 
+    # error check values, no negatives and must be in range
+# Output the numbers in a spiral pattern (clockwise), no space or hyphens
+# 1 = one, 2 = two, 49 = fortynine
+    # starting above the upper left corner of the previously completed rectangle
+    # keep going until one has drawn a rectangle for each num from start
+    # to stop input values
+# Program should ask the user if they want to run the program again
+    # check case 
